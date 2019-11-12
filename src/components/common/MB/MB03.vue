2b85742b39fbd582d3051fdad62bee20e8901440
Shallny <1043611591@qq.com>
2019-11-12 11:34:39 +0800

    添加视频新模块

---
+++ src/components/common/MB/MB03.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div class="mb-main">
+        <component :is="MC13"></component>
+    </div>
+</template>
+
+<script>
+import MC13 from '../../../components/common/MC/MC13';
+export default {
+    name:'MB03',
+     components:{
+          MC13,
+     }
+}
+</script>
+
+<style scoped>
+
+</style>

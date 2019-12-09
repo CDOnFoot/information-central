b0b3d7449c956325760349ab95eb3c4ffa481ac0
guoru <youguo07@163.com>
2019-12-09 17:47:59 +0800

    修改登录页面样式，更改为3D样式，重新修改背景图片及相关内容

--- src/components/page/login.vue
+++ src/components/page/login.vue
@@ -2,10 +2,12 @@
   <div>
     <div class="main-content">
       <img src="../../assets/img/logo.png" alt="" class="img-logo" ondragstart="return false" />
+      <div id="indexLizi" />
+
       <div class="form">
         <div class="form-detail">
           <div class="title">
-              信息中心
+              信  息  中  心
           </div>
           <div class="form-content">
             
@@ -40,7 +42,7 @@
       </div>
       <!-- <img src="../../assets/img/sun.png" alt="" class="sun-btn-img"> -->
       <!--动效区域-->
-      <div :class="'trangle'+index " class="trangle" v-for="(item,index) of trangleList" :key="index"></div>
+      <!-- <div :class="'trangle'+index " class="trangle" v-for="(item,index) of trangleList" :key="index"></div> -->
       <div class="copyrights">
         <p class="corporate-footer">© 2019 UCD. All Rights Reserved.</p>
       </div>
@@ -53,10 +55,42 @@
 import Cookies from 'js-cookie'
 import preventBack from 'vue-prevent-browser-back';
 import sha256 from "js-sha256";
+import * as THREE from 'three'
   export default {
     name: "login",
+    props: {
+      amountX: {
+        type: Number,
+        default: 50
+      },
+      amountY: {
+        type: Number,
+        default: 50
+      },
+      color: {
+        type: Number,
+        default: 0x2d5097
+      },
+      top: {
+        type: Number,
+        default: 350
+      }
+    },
     data(){
       return{
+        count: 0,
+        // 用来跟踪鼠标水平位置
+        mouseX: 0,
+        windowHalfX: null,
+        // 相机
+        camera: null,
+        // 场景
+        scene: null,
+        // 批量管理粒子
+        particles: null,
+        // 渲染器
+        renderer: null,
+
         trangleList:[0,1,2,3,4,5,6],
         userName: '',
         userPassword: '',
@@ -71,9 +105,132 @@ import sha256 from "js-sha256";
     mixins: [preventBack],//注入
     mounted(){
       this.$router.push("/login");
+      this.init();
+      this.animate();
     },
 
     methods:{
+
+      init: function() {
+      const SEPARATION = 100
+      const SCREEN_WIDTH = window.innerWidth
+      const SCREEN_HEIGHT = window.innerHeight
+      const container = document.createElement('div')
+      this.windowHalfX = window.innerWidth / 2
+      container.style.position = 'relative'
+      container.style.top = `${this.top}px`
+      container.style.height = `${(SCREEN_HEIGHT - this.top)}px`
+      document.getElementById('indexLizi').appendChild(container)
+
+      this.camera = new THREE.PerspectiveCamera(75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000)
+      this.camera.position.z = 1000
+
+      this.scene = new THREE.Scene()
+
+      const numParticles = this.amountX * this.amountY
+      const positions = new Float32Array(numParticles * 3)
+      const scales = new Float32Array(numParticles)
+      // 初始化粒子位置和大小
+      let i = 0
+      let j = 0
+      for (let ix = 0; ix < this.amountX; ix++) {
+        for (let iy = 0; iy < this.amountY; iy++) {
+          positions[i] = ix * SEPARATION - ((this.amountX * SEPARATION) / 2)
+          positions[i + 1] = 0
+          positions[i + 2] = iy * SEPARATION - ((this.amountY * SEPARATION) / 2)
+          scales[j] = 1
+          i += 3
+          j++
+        }
+      }
+
+      const geometry = new THREE.BufferGeometry()
+      geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3))
+      geometry.addAttribute('scale', new THREE.BufferAttribute(scales, 1))
+      // 初始化粒子材质
+      const material = new THREE.ShaderMaterial({
+        uniforms: {
+          color: { value: new THREE.Color(this.color) }
+        },
+        vertexShader: `
+          attribute float scale;
+          void main() {
+            vec4 mvPosition = modelViewMatrix * vec4( position, 2.0 );
+            gl_PointSize = scale * ( 300.0 / - mvPosition.z );
+            gl_Position = projectionMatrix * mvPosition;
+          }
+        `,
+        fragmentShader: `
+          uniform vec3 color;
+          void main() {
+            if ( length( gl_PointCoord - vec2( 0.5, 0.5 ) ) > 0.475 ) discard;
+            gl_FragColor = vec4( color, 1.0 );
+          }
+        `
+      })
+
+      this.particles = new THREE.Points(geometry, material)
+      this.scene.add(this.particles)
+
+      this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+      this.renderer.setSize(container.clientWidth, container.clientHeight)
+      this.renderer.setPixelRatio(window.devicePixelRatio)
+      this.renderer.setClearAlpha(0)
+      container.appendChild(this.renderer.domElement)
+
+      window.addEventListener('resize', this.onWindowResize, { passive: false })
+      document.addEventListener('mousemove', this.onDocumentMouseMove, { passive: false })
+      document.addEventListener('touchstart', this.onDocumentTouchStart, { passive: false })
+      document.addEventListener('touchmove', this.onDocumentTouchMove, { passive: false })
+    },
+    render: function() {
+      this.camera.position.x += (this.mouseX - this.camera.position.x) * 0.05
+      this.camera.position.y = 400
+      this.camera.lookAt(this.scene.position)
+      const positions = this.particles.geometry.attributes.position.array
+      const scales = this.particles.geometry.attributes.scale.array
+      // 计算粒子位置及大小
+      let i = 0
+      let j = 0
+      for (let ix = 0; ix < this.amountX; ix++) {
+        for (let iy = 0; iy < this.amountY; iy++) {
+          positions[i + 1] = (Math.sin((ix + this.count) * 0.3) * 100) + (Math.sin((iy + this.count) * 0.5) * 100)
+          scales[j] = (Math.sin((ix + this.count) * 0.3) + 1) * 8 + (Math.sin((iy + this.count) * 0.5) + 1) * 8
+          i += 3
+          j++
+        }
+      }
+      // 重新渲染粒子
+      this.particles.geometry.attributes.position.needsUpdate = true
+      this.particles.geometry.attributes.scale.needsUpdate = true
+      this.renderer.render(this.scene, this.camera)
+      this.count += 0.1
+    },
+    animate: function() {
+      requestAnimationFrame(this.animate)
+      this.render()
+    },
+    onDocumentMouseMove: function(event) {
+      this.mouseX = event.clientX - this.windowHalfX
+    },
+    onDocumentTouchStart: function(event) {
+      if (event.touches.length === 1) {
+        this.mouseX = event.touches[0].pageX - this.windowHalfX
+      }
+    },
+    onDocumentTouchMove: function(event) {
+      if (event.touches.length === 1) {
+        event.preventDefault()
+        this.mouseX = event.touches[0].pageX - this.windowHalfX
+      }
+    },
+    onWindowResize: function() {
+      this.windowHalfX = window.innerWidth / 2
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+  },
  
       // ...mapMutations(['changeLogin']),//存储login状态到vuex
 
@@ -157,11 +314,17 @@ import sha256 from "js-sha256";
         });
       }
     }
-  }
+  
 </script>
 
 <style scoped>
+.ant-input-affix-wrapper{
+    border-radius: 5px !important;
+}
 
+.ant-btn-primary{
+  background-color: #005db3 !important;
+}
   .main-content{
     position: absolute;
     height: 100%;
@@ -170,13 +333,6 @@ import sha256 from "js-sha256";
     overflow: hidden;
     background: url(../../assets/img/login-bg.png) center center no-repeat #000d19;
     background-size: 100%;
-  }
-
-  .bg{
-    position: relative;
-    width: 100%;
-    height: auto;
-    z-index: 1;
   }
   .img-logo{
     position: absolute;
@@ -197,21 +353,19 @@ import sha256 from "js-sha256";
     width: 100%;
     height: auto;
   }
-  .title{
-    font-size: 20px;
-    text-align: center;
-    margin: 0 auto 4px auto;
-    /* color: #304b65; */
-    color: #ffffff;
-    margin-bottom: 20px;
-    font-weight: bold;
-  }
+  .title {
+      font-size: 26px;
+      color: #eee;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
   .detail-title{
     font-size:22px;
     color:rgba(14,139,255,1);
     line-height:42px;
     text-align: center;
-    margin-bottom: 5px;
+    /* margin-bottom: 5px; */
   }
   .detail-title span{
     font-size:14px;
@@ -222,18 +376,19 @@ import sha256 from "js-sha256";
   .form-detail{
     position: relative;
     z-index: 199;
-    padding: 10px;
-    width: 320px;
-    margin-left: 250px;
-    border-radius: 4px;
-    border: 1px solid #0f3b5b;
-    background: #0f3b5b;
+    /* padding: 10px; */
+    width: 398px;
+    margin-left: -168px;
+    /* border-radius: 4px; */
+    /* border: 1px solid #0f3b5b; */
+    /* background: #0f3b5b; */
     overflow: hidden;
-    color: #303133;
-    -webkit-transition: 0.3s;
+    /* color: #303133; */
+    /* -webkit-transition: 0.3s; */
     transition: 0.3s;
-    box-shadow: 0 0px 10px 2px rgba(255, 255, 255, 0.39);
+    /* box-shadow: 0 0px 10px 2px rgba(255, 255, 255, 0.39); */
   }
+  
   .form-content{
     position: relative;
     text-align: center;
@@ -253,7 +408,6 @@ import sha256 from "js-sha256";
   }
   .login-register{
     position: relative;
-    /* margin-top: 20px; */
     cursor: pointer;
     font-size: 14px;
     float: right;
@@ -265,6 +419,8 @@ import sha256 from "js-sha256";
     color: #ccc;
     transition: color 0.3s;
     font-size: 12px;
+    float: right;
+    background: #f09 !important;
   }
   .components-input-demo-presuffix .anticon-close-circle:hover {
     color: #999;
@@ -273,82 +429,14 @@ import sha256 from "js-sha256";
     color: #666;
   }
   .btn-login{
-    margin-top: 3%;
     width: 100%;
     height: 40px;
-  }
-  .sun-btn-img{
-     position: absolute;
-    top: 37%;
-    left: 59.3%;
-    z-index: 19;
   }
   .ant-input-affix-wrapper{
       font-size: 16px;
       color: #000;
   }
-  .trangle{
-    background: rgba(180, 182, 183, 0.15);
-    position: absolute;
-    top: -1080px;
-    z-index: 19;
-  }
-  .trangle0{
-    width: 70px;
-    height: 70px;
-    left: 52%;
-    animation: rolling 5s linear infinite;
-    border-radius: 28px;
 
-  }
-  .trangle1{
-    width: 60px;
-    height: 60px;
-    left: 15%;
-    animation: rolling 6s linear infinite;
-    border-radius: 10px;
-
-  }
-  .trangle2{
-    width: 86px;
-    height: 86px;
-    left: 89%;
-    animation: rolling 8s linear infinite;
-    border-radius: 20px;
-
-  }
-  .trangle3{
-    width: 20px;
-    height: 20px;
-    left: 48.5%;
-    animation: rolling 6s linear infinite;
-    border-radius: 4px;
-
-  }
-  .trangle4{
-    width: 50px;
-    height: 50px;
-    left: 78%;
-    animation: rolling 4s linear infinite;
-    border-radius: 10px;
-
-  }
-  .trangle5{
-    width: 30px;
-    height: 30px;
-    left: 40%;
-    animation: rolling 7s linear infinite;
-    border-radius: 10px;
-
-  }
-  .trangle6{
-    width: 80px;
-    height: 80px;
-    left: 6%;
-    animation: rolling 5s linear infinite;
-    border-radius: 10px;
-
-  }
   @keyframes rolling {
     0%{
       transform:rotate(0deg);
@@ -374,7 +462,16 @@ import sha256 from "js-sha256";
   }
   .ant-input-affix-wrapper .ant-input:not(:first-child) {
       padding-left: 40px !important;
+    background: #f09 !important;
+
   }
+#indexLizi {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  overflow: hidden;
+}
 </style>
 
       // emitEmpty (e) {

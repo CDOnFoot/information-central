254bad92aedb20be6b97d3f6b489a92553290df4
xuyuxiang <553283170@qq.com>
2019-12-10 15:32:54 +0800

    优化首页echarts图表，增加确认报警的交互，日常bug的修复

--- src/components/common/MC/MC02.vue
+++ src/components/common/MC/MC02.vue
@@ -42,6 +42,61 @@ export default {
   methods: {
     initChart () {
       let chartInit = this.$echarts.init(document.getElementById('chart-0'));
+
+      let option = {
+        /*title: {
+          text: '当前电量',
+          textStyle: {
+            color: '#fff',
+            // fontStyle: 'italic',
+            fontWeight: 'bold',
+            fontFamily: '黑体'
+          }
+        },*/
+
+        // color: ['#365fb4'],
+
+        series: [
+          // 外圈 - 饼 pie
+          {
+            type: 'pie',
+            name: '当前电量',
+            radius: ['65%', '80%'],
+            startAngle: 270,
+
+            label: {
+              show: true,
+              formatter: '{b}:{c}%',
+            },
+
+            itemStyle: {
+              color: '#f82',
+              borderColor: '#fff',
+              borderWidth: 10,
+            },
+
+            center: ['50%', '50%'],
+
+            data: [
+              {
+                name: '当前电量',
+                value: 50
+              },
+              {
+                name: '',
+                value: 50
+              }
+            ]
+          },
+
+          // 内圈 - 基于仪表盘
+          {
+            type: 'gauge'
+          }
+        ]
+      };
+
+      chartInit.setOption(option)
     }
   }
 };
@@ -64,5 +119,7 @@ export default {
   font-size: 14px;
   text-align: center;
   padding-top: 20px;
+  width: 100%;
+  height: 100%;
 }
 </style>

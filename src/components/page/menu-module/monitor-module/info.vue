37a316c865f0475bc9da28e49a7748ba13cb8be9
wei <790979760@qq.com>
2019-12-10 09:38:48 +0800

    增加空调视图基本信息空调视图概览

--- src/components/page/menu-module/monitor-module/info.vue
+++ src/components/page/menu-module/monitor-module/info.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="info">
-    <div>
+    <div class="info-bg">
 
     </div>
-
     <div class="info-pic"></div>
     <div class="info-list">
-      <a-table :showHeader="false" :columns="columns" :dataSource="data" :pagination="pagination" :loading="loading"
-        :customRow="setRow">
-        <!-- <template slot="name" slot-scope="name">{{name.first}} {{name.last}}</template> -->
+      <a-table :showHeader="false" :columns="columns" :dataSource="data" :pagination="false" :loading="loading"
+               :customRow="setRow">
       </a-table>
     </div>
   </div>
 </template>
 
 <script>
-  const columns = [{
+  const columns = [
+    {
       dataIndex: "name",
       width: "20%",
       align: "center"
     },
-    { 
+    {
       dataIndex: "data",
       width: "20%",
       align: "center"
@@ -28,9 +27,9 @@
   ];
 
   const data = [{
-      name: "软件版本号",
-      data: "PUN-UU 091111"
-    },
+    name: "软件版本号",
+    data: "PUN-UU 091111"
+  },
 
     {
       name: "软件版本号",
@@ -70,7 +69,26 @@
     {
       name: "软件版本号",
       data: "PUN-UU 091111"
-    }
+    },
+    {
+      name: "软件版本号",
+      data: "PUN-UU 091111"
+    },
+
+    {
+      name: "软件版本号",
+      data: "PUN-UU 091111"
+    },
+
+    {
+      name: "软件版本号",
+      data: "PUN-UU 091111"
+    },
+
+    {
+      name: "软件版本号",
+      data: "PUN-UU 091111"
+    },
   ];
 
   export default {
@@ -81,13 +99,13 @@
         pagination: {
           current: 1,
           defaultCurrent: 1,
-          defaultPageSize: 10,
+          defaultPageSize: 20,
           total: 0,
           showQuickJumper: true,
           onChange: current => this.changePage(current)
         },
         loading: false,
-        columns
+        columns,
       };
     },
     mounted() {
@@ -103,17 +121,23 @@
         this.pagination.current = page;
       },
       setRow(record, index) {
-        if (index % 2 == 1) {
-          console.log(index);
-        }
         return {
           on: {
             props: {},
             click: () => {
               console.log(record);
+              document.getElementsByClassName('ant-table-row')[index].style.backgroundColor = 'red';
+
             }
           }
         };
+        if (index % 2 == 1) {
+          console.log(index);
+        } else {
+          // console.log(2)
+          document.getElementsByClassName('ant-table-row')[index].style.backgroundColor = 'red';
+
+        }
       }
     }
   };
@@ -124,22 +148,46 @@
   .info {
     display: flex;
     height: 100%;
+    position: relative;
+  }
+
+  .info-bg {
+    z-index: 0;
+    position: absolute;
+    display: flex;
+
+    width: 100%;
+    height: 100%;
+    background-color: #2250ca;
+    opacity: .2;
   }
 
   .info-pic {
+    z-index: 10;
+
     flex: 0 60%;
     height: 100%;
     padding: 20px;
-        background: url("../../../../assets/img/monitor/dev3.png") no-repeat center;
-        background-size:36%;
-
-     /* background: url(bgimage.gif) no-repeat fixed top; */
+    background: url("../../../../assets/img/monitor/dev3.png") no-repeat center;
+    background-size: 42%;
   }
 
   .info-list {
-    flex: 0 30%;
+    z-index: 10;
+
+    flex: 0 40%;
     height: 100%;
-    padding: 20px;
+    padding: 0 60px 0 60px;
+
+
   }
 
+  /*/deep/ .ant-table-pagination.ant-pagination {*/
+  /*visibility: hidden;*/
+  /*}*/
+
+  /deep/ .ant-table-row {
+    /*background-color: #315c85;*/
+    /*margin-bottom: 10px !important;*/
+  }
 </style>
